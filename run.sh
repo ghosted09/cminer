145af1a55f4a330ba1b108e83d63f9d2e299bcd8
ghosted09 <43025834+ghosted09@users.noreply.github.com>
2024-04-30 22:53:59 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #exit existing screens with the name CCminer
-screen -S CCminer -X quit 1>/dev/null 2>&1
+screen -S luckpool -X quit 1>/dev/null 2>&1
 #wipe any existing (dead) screens)
 screen -wipe 1>/dev/null 2>&1
 #create new disconnected session CCminer
 screen -dmS CCminer 1>/dev/null 2>&1
 #run the miner
-screen -S CCminer -X stuff "~/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3960 -u RS16FKw6kF8bTEn33SDtUsUfJiwY2X4usD.Arm -p x -t 4\n" 1>/dev/null 2>&1
+screen -S CCminer -X stuff "~/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3960 -u RS16FKw6kF8bTEn33SDtUsUfJiwY2X4usD.Arm -p x -t 2\n" 1>/dev/null 2>&1
 printf '\nMining started.\n'
 printf '===============\n'
 printf '\nManual:\n'
