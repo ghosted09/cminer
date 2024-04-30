319f3faa60ba8629a69780f02dcab577bd494b1b
ghosted09 <43025834+ghosted09@users.noreply.github.com>
2024-04-30 22:55:11 +0700

    Update starting.sh

--- starting.sh
+++ starting.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #exit existing screens with the name CCminer
-screen -S CCminer -X quit 1>/dev/null 2>&1
+screen -S vipor -X quit 1>/dev/null 2>&1
 #wipe any existing (dead) screens)
 screen -wipe 1>/dev/null 2>&1
 #create new disconnected session CCminer
-screen -dmS CCminer 1>/dev/null 2>&1
+screen -dmS vipor 1>/dev/null 2>&1
 #run the miner
-screen -S CCminer -X stuff "~/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5045 -u RS16FKw6kF8bTEn33SDtUsUfJiwY2X4usD.Arm -p x -t 4\n" 1>/dev/null 2>&1
+screen -S vipor -X stuff "~/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5045 -u RS16FKw6kF8bTEn33SDtUsUfJiwY2X4usD.Arm -p x -t 2\n" 1>/dev/null 2>&1
 printf '\nMining started.\n'
 printf '===============\n'
 printf '\nManual:\n'
